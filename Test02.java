1fb81d73d2bd6710dc60fcb8ecbca20775228533
chzlikejava <49946037+chzlikejava@users.noreply.github.com>
2019-04-24 20:03:24 +0800

    firstDay
    第一天

---
+++ Test02.java
@@ -0,0 +1,27 @@
+package helloWorld;
+
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i1=88,i2=-88;
+		double d =88.888;
+		char ch1 = 'A',ch2 = '8';
+		boolean flag1 = true,flag2 = false;
+		System.out.println("i1:"+i1+",i2:"+i2);
+		System.out.println("d:"+d);
+		System.out.println("ch1:"+ch1+",ch2:"+ch2);
+		System.out.println("flag1:"+flag1+",flag2:"+flag2);
+		System.out.println("好好学习~");
+		System.out.println("天天向上~");
+		System.out.println("键盘敲烂，月薪过万~~");
+		//玫瑰花。
+		System.out.println("{@}");
+		System.out.println("/|\\");
+		System.out.println(" |");
+		
+	}
+	
+
+}
